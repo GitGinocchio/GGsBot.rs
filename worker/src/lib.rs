fae7c0966a171e34ca224abf1167856ac82ed935
GitGinocchio <106624847+GitGinocchio@users.noreply.github.com>
2026-07-19 16:21:00 +0200

    refactor: removed durable object

--- worker/src/lib.rs
+++ worker/src/lib.rs
@@ -10,9 +10,9 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
     Router::new()
         .post_async("/api/interaction", async |req, env| { bot.handle_interaction(req, env.env).await })
         .on_async("/api/*path", async |req, env| { bot.handle_api(req, env.env).await })
-        
-        .or_else_any_method_async("/*path", async |_, _| { Response::error("Method not allowed", 405)})
+
         .on_async("/*path", async |_, _| { Response::error("Not Found", 404)})
+        .or_else_any_method_async("/*path", async |_, _| { Response::error("Method not allowed", 405)})
         
         .run(req, env)
         .await
